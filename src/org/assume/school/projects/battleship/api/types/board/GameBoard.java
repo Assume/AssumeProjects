6b7190b0144cf64dcaa43b572b58857ebc5b7dbf
Adam <one@pbot.org>
2013-11-23 15:29:38 -0800

    stuff relating to moving

--- src/org/assume/school/projects/battleship/api/types/board/GameBoard.java
+++ src/org/assume/school/projects/battleship/api/types/board/GameBoard.java
@@ -10,166 +10,221 @@ import org.assume.school.projects.battleship.api.types.users.Player;
 public class GameBoard implements Playable
 {
 
-	public static void main(String[] args)
+    public static void main(String[] args)
+    {
+
+	Player p = new Player("Adam");
+	Player p2 = new Player("Roy");
+
+	p.setOpponent(p2);
+	p2.setOpponent(p);
+	AircraftCarrier.createInstance(p);
+	System.out.println(p);
+	p.executeCommand(new MoveCommand(MoveCommand.DOWN, p.getShips().get(0),
+		p));
+	System.out.println(p);
+
+    }
+
+    private final Location[][] grid;
+
+    public GameBoard()
+    {
+	this.grid = Location.getFullGrid(10, 10);
+    }
+
+    @Override
+    public String isShipPlacementValid(int row, int col, int orientation,
+	    int size)
+    {
+	if (orientation == Ship.VERTICAL)
+	{
+	    if (row + size > this.grid.length)
+		return "Invalid. Ship will be off board! loc[r" + row + ",c"
+			+ col + "]";
+	    for (int i = 0; i < size; i++)
+	    {
+		if (!this.grid[row + i][col].getState().equals(
+			LocationState.EMPTY))
+		    return "Invalid. Space already taken by other ship! loc[r"
+			    + row + ",c" + col + "]";
+	    }
+	} else if (orientation == Ship.HORIZONTAL)
+	{
+	    if (col + size > 10)
+		return "Invalid. Ship will be off board! loc[r" + row + ",c"
+			+ col + "]";
+	    for (int i = 0; i < size; i++)
+	    {
+		if (!this.grid[row][col + i].getState().equals(
+			LocationState.EMPTY))
+		    return "Invalid. Space already taken by other ship! loc[r"
+			    + row + ",c" + col + "]";
+	    }
+	}
+	return "Valid";
+    }
+
+    @Override
+    public boolean isCodeValid(String code)
+    {
+	return code.equals("Valid");
+    }
+
+    @Override
+    public boolean isAttackValid(int row, int col, Player attackee)
+    {
+	for (Ship s : attackee.getShips())
+	{
+	    if (s.doesPegHit(row, col, this))
+		return true;
+	}
+	return false;
+    }
+
+    @Override
+    public boolean isProbeEmpty(int topRow, int bottomRow, int leftCol,
+	    int rightCol, Player probee)
+    {
+	return false;
+    }
+
+    @Override
+    public Ship getShipAt(int row, int col, Player probee)
+    {
+	return probee.getShipAt(row, col);
+    }
+
+    public Location[][] getGrid()
+    {
+	return grid;
+    }
+
+    @Override
+    public void addShip(Ship s)
+    {
+	if (s.getOrientation() == Ship.VERTICAL)
+	{
+	    for (int i = 0; i < s.getSize(); i++)
+	    {
+		this.grid[s.getRow() + i][s.getCol()]
+			.setState(LocationState.SHIP_PART);
+	    }
+	} else if (s.getOrientation() == Ship.HORIZONTAL)
+	{
+	    for (int i = 0; i < s.getSize(); i++)
+	    {
+		this.grid[s.getRow()][s.getCol() + i]
+			.setState(LocationState.SHIP_PART);
+	    }
+	}
+    }
+
+    @Override
+    public String toString()
+    {
+	StringBuilder builder = new StringBuilder();
+	builder.append("B ");
+	for (int i = 0; i < 10; i++)
+	    builder.append(i + " ");
+	builder.append("\n");
+	for (int i = 0; i < 10; i++)
+	{
+	    builder.append(i);
+	    for (int j = 0; j < 10; j++)
+	    {
+		builder.append(" " + this.grid[i][j].getState().getChar());
+	    }
+	    builder.append("\n");
+	}
+
+	return builder.toString();
+    }
+
+    @Override
+    public void onHit(int row, int col)
+    {
+	this.grid[row][col].setState(LocationState.SHIP_HIT);
+
+    }
+
+    @Override
+    public void onMiss(int row, int col)
+    {
+	this.grid[row][col].setState(LocationState.PEG);
+
+    }
+
+    private void moveShipUp(Ship ship)
+    {
+	if (ship.getOrientation() == Ship.VERTICAL)
+	{
+	    for (int i = ship.getRow(); i < ship.getRow() + ship.getSize(); i++)
+		this.grid[i][ship.getCol()].setState(this.grid[i + 1][ship
+			.getCol()].getState());
+	    this.grid[ship.getRow() + ship.getSize()][ship.getCol()]
+		    .setState(LocationState.EMPTY);
+	} else if (ship.getOrientation() == Ship.HORIZONTAL)
+	{
+	    for (int i = ship.getCol(); i < ship.getCol() + ship.getSize(); i++)
+		this.grid[ship.getRow()][i]
+			.setState(this.grid[ship.getRow() + 1][ship.getCol()]
+				.getState());
+	    for (int i = ship.getCol(); i < ship.getCol() + ship.getSize(); i++)
+		this.grid[ship.getRow() + 1][i].setState(LocationState.EMPTY);
+	}
+    }
+
+    private void moveShipDown(Ship ship)
+    {
+	if (ship.getOrientation() == Ship.VERTICAL)
+	{
+	    for (int i = ship.getRow(); i < ship.getRow() + ship.getSize(); i++)
+		this.grid[i][ship.getCol()].setState(this.grid[i - 1][ship
+			.getCol()].getState());
+	    this.grid[ship.getRow() - 1][ship.getCol()]
+		    .setState(LocationState.EMPTY);
+	} else if (ship.getOrientation() == Ship.HORIZONTAL)
+	{
+	    for (int i = ship.getCol(); i < ship.getCol() + ship.getSize(); i++)
+		this.grid[ship.getRow()][i]
+			.setState(this.grid[ship.getRow() - 1][ship.getCol()]
+				.getState());
+	    for (int i = ship.getCol(); i < ship.getCol() + ship.getSize(); i++)
+		this.grid[ship.getRow() - 1][i].setState(LocationState.EMPTY);
+	}
+
+    }
+
+    private void moveShipRight(Ship ship)
+    {
+	if (ship.getOrientation() == Ship.VERTICAL)
+	{
+	    for (int i = ship.getCol(); i < ship.getCol() + ship.getSize(); i++)
+		this.grid[ship.getRow()][i]
+			.setState(this.grid[ship.getRow()][i - 1].getState());
+	    this.grid[ship.getRow()][ship.getCol() - 1]
+		    .setState(LocationState.EMPTY);
+	} else if (ship.getOrientation() == Ship.HORIZONTAL)
 	{
 
-		Player p = new Player("Adam");
-		Player p2 = new Player("Roy");
+	}
+    }
 
-		p.setOpponent(p2);
-		p2.setOpponent(p);
-		AircraftCarrier.createInstance(p);
-		System.out.println(p);
-		p.executeCommand(new MoveCommand(MoveCommand.UP, p.getShips().get(0), p));
-		System.out.println(p);
+    @Override
+    public void moveShip(Ship ship, int direction)
+    {
+	if (direction == MoveCommand.UP)
+	{
+	    this.moveShipUp(ship);
+
+	} else if (direction == MoveCommand.DOWN)
+	{
+	    this.moveShipDown(ship);
+	} else if (direction == MoveCommand.RIGHT)
+	{
 
 	}
 
-	private final Location[][] grid;
-
-	public GameBoard()
-	{
-		this.grid = Location.getFullGrid(10, 10);
-	}
-
-	@Override
-	public String isShipPlacementValid(int row, int col, int orientation,
-			int size)
-	{
-		if (orientation == Ship.VERTICAL)
-		{
-			if (row + size > this.grid.length)
-				return "Invalid. Ship will be off board! loc[r" + row + ",c"
-						+ col + "]";
-			for (int i = 0; i < size; i++)
-			{
-				if (!this.grid[row + i][col].getState().equals(
-						LocationState.EMPTY))
-					return "Invalid. Space already taken by other ship! loc[r"
-							+ row + ",c" + col + "]";
-			}
-		}
-		else if (orientation == Ship.HORIZONTAL)
-		{
-			if (col + size > 10)
-				return "Invalid. Ship will be off board! loc[r" + row + ",c"
-						+ col + "]";
-			for (int i = 0; i < size; i++)
-			{
-				if (!this.grid[row][col + i].getState().equals(
-						LocationState.EMPTY))
-					return "Invalid. Space already taken by other ship! loc[r"
-							+ row + ",c" + col + "]";
-			}
-		}
-		return "Valid";
-	}
-
-	@Override
-	public boolean isCodeValid(String code)
-	{
-		return code.equals("Valid");
-	}
-
-	@Override
-	public boolean isAttackValid(int row, int col, Player attackee)
-	{
-		for (Ship s : attackee.getShips())
-		{
-			if (s.doesPegHit(row, col, this)) return true;
-		}
-		return false;
-	}
-
-	@Override
-	public boolean isProbeEmpty(int topRow, int bottomRow, int leftCol,
-			int rightCol, Player probee)
-	{
-		return false;
-	}
-
-	@Override
-	public Ship getShipAt(int row, int col, Player probee)
-	{
-		return probee.getShipAt(row, col);
-	}
-
-	public Location[][] getGrid()
-	{
-		return grid;
-	}
-
-	@Override
-	public void addShip(Ship s)
-	{
-		if (s.getOrientation() == Ship.VERTICAL)
-		{
-			for (int i = 0; i < s.getSize(); i++)
-			{
-				this.grid[s.getRow() + i][s.getCol()]
-						.setState(LocationState.SHIP_PART);
-			}
-		}
-		else if (s.getOrientation() == Ship.HORIZONTAL)
-		{
-			for (int i = 0; i < s.getSize(); i++)
-			{
-				this.grid[s.getRow()][s.getCol() + i]
-						.setState(LocationState.SHIP_PART);
-			}
-		}
-	}
-
-	@Override
-	public String toString()
-	{
-		StringBuilder builder = new StringBuilder();
-		builder.append("B ");
-		for (int i = 0; i < 10; i++)
-			builder.append(i + " ");
-		builder.append("\n");
-		for (int i = 0; i < 10; i++)
-		{
-			builder.append(i);
-			for (int j = 0; j < 10; j++)
-			{
-				builder.append(" " + this.grid[i][j].getState().getChar());
-			}
-			builder.append("\n");
-		}
-
-		return builder.toString();
-	}
-
-	@Override
-	public void onHit(int row, int col)
-	{
-		this.grid[row][col].setState(LocationState.SHIP_HIT);
-
-	}
-
-	@Override
-	public void onMiss(int row, int col)
-	{
-		this.grid[row][col].setState(LocationState.PEG);
-
-	}
-
-	@Override
-	public void moveShip(Ship ship, int direction)
-	{
-		if (direction == MoveCommand.UP)
-		{
-			for (int i = ship.getRow(); i < ship.getRow() + ship.getSize(); i++)
-				this.grid[i][ship.getCol()].setState(this.grid[i + 1][ship
-						.getCol()].getState());
-			this.grid[ship.getRow() + ship.getSize()][ship.getCol()]
-					.setState(LocationState.EMPTY);
-		}
-		else if (direction == MoveCommand.DOWN)
-			for (int i = ship.getRow(); i < ship.getRow() + ship.getSize(); i++)
-				this.grid[i][ship.getCol()].setState(this.grid[i - 1][ship
-						.getCol()].getState());
-	}
-
+    }
 }
--- src/org/assume/school/projects/battleship/api/types/command/MoveCommand.java
+++ src/org/assume/school/projects/battleship/api/types/command/MoveCommand.java
@@ -8,62 +8,110 @@ import org.assume.school.projects.battleship.api.types.users.Player;
 public class MoveCommand implements Commandable
 {
 
-	public static final int UP = 1;
-	public static final int DOWN = 2;
+    public static final int UP = 1;
+    public static final int DOWN = 2;
 
-	public static final int LEFT = 3;
-	public static final int RIGHT = 4;
+    public static final int LEFT = 3;
+    public static final int RIGHT = 4;
 
-	private int direction;
-	private Ship ship;
-	private Player player;
+    private int direction;
+    private Ship ship;
+    private Player player;
 
-	public MoveCommand(int direction, Ship ship, Player player)
+    public MoveCommand(int direction, Ship ship, Player player)
+    {
+	this.direction = direction;
+	this.ship = ship;
+	this.player = player;
+    }
+
+    private boolean canExecuteUp()
+    {
+	if (this.ship.getOrientation() == Ship.VERTICAL)
 	{
-		this.direction = direction;
-		this.ship = ship;
-		this.player = player;
-	}
-
-	@Override
-	public boolean canExecute()
+	    return ((this.ship.getRow() - 1) >= 0)
+		    && player.getBoard().getGrid()[ship.getRow() - 1][ship
+			    .getCol()].getState().equals(LocationState.EMPTY);
+	} else if (this.ship.getOrientation() == Ship.HORIZONTAL)
 	{
-		if (this.direction == UP)
-			return ((ship.getRow() - 1) >= 0) && player.getBoard().getGrid()[ship.getRow() - 1][ship.getCol()].getState().equals(LocationState.EMPTY);
-		else if (this.direction == DOWN)
-			return ((ship.getRow() + 1) <= 9) && player.getBoard().getGrid()[ship.getRow() + 1][ship.getCol()].getState().equals(LocationState.EMPTY);
-		else if (this.direction == RIGHT)
-			return ((ship.getCol() + 1) <= 9) && player.getBoard().getGrid()[ship.getRow()][ship.getCol() + 1].getState().equals(LocationState.EMPTY);
-		else if (this.direction == LEFT)
-			return ((ship.getCol() - 1) >= 0) && player.getBoard().getGrid()[ship.getRow()][ship.getCol() + -1].getState().equals(LocationState.EMPTY);
+	    if ((this.ship.getRow() - 1) < 0)
 		return false;
+	    for (int i = 0; i < this.ship.getSize(); i++)
+	    {
+		if (!this.player.getBoard().getGrid()[this.ship.getRow() - 1][this.ship
+			.getCol() + i].getState().equals(LocationState.EMPTY))
+		    return false;
+	    }
 	}
+	return true;
+    }
 
-	@Override
-	public void execute()
+    private boolean canExecuteDown()
+    {
+	if (this.ship.getOrientation() == Ship.VERTICAL)
 	{
-		if (!canExecute()) return;
-
-		if (this.direction == UP)
-		{
-			ship.setRow(ship.getRow() - 1);
-			this.player.getBoard().moveShip(this.ship, direction);
-		}
-		else if (this.direction == DOWN)
-		{
-			ship.setRow(ship.getRow() + 1);
-			this.player.getBoard().moveShip(this.ship, direction);
-		}
-		else if (this.direction == RIGHT)
-		{
-			ship.setCol(ship.getCol() + 1);
-			this.player.getBoard().moveShip(this.ship, direction);
-		}
-		else if (this.direction == LEFT)
-		{
-			ship.setCol(ship.getCol() - 1);
-			this.player.getBoard().moveShip(this.ship, direction);
-		}
+	    return ((this.ship.getRow() + ship.getSize() + 1) <= 9)
+		    && player.getBoard().getGrid()[ship.getRow()
+			    + ship.getSize() + 1][ship.getCol()].getState()
+			    .equals(LocationState.EMPTY);
+	} else if (this.ship.getOrientation() == Ship.HORIZONTAL)
+	{
+	    if ((this.ship.getRow() - 1) < 0)
+		return false;
+	    for (int i = 0; i < this.ship.getSize(); i++)
+	    {
+		if (!this.player.getBoard().getGrid()[this.ship.getRow() + 1][this.ship
+			.getCol() + i].getState().equals(LocationState.EMPTY))
+		    return false;
+	    }
 	}
+	return true;
+    }
+
+    @Override
+    public boolean canExecute()
+    {
+	if (this.direction == UP)
+	{
+	    return this.canExecuteUp();
+	} else if (this.direction == DOWN)
+	{
+	    return this.canExecuteDown();
+	} else if (this.direction == RIGHT)
+	    return ((this.ship.getCol() + ship.getSize() + 1) <= 9)
+		    && player.getBoard().getGrid()[ship.getRow()][ship.getCol()
+			    + ship.getSize() + 1].getState().equals(
+			    LocationState.EMPTY);
+	else if (this.direction == LEFT)
+	    return ((this.ship.getCol() - 1) >= 0)
+		    && player.getBoard().getGrid()[ship.getRow()][ship.getCol()
+			    + -1].getState().equals(LocationState.EMPTY);
+	return false;
+    }
+
+    @Override
+    public void execute()
+    {
+	if (!canExecute())
+	    return;
+
+	if (this.direction == UP)
+	{
+	    ship.setRow(ship.getRow() - 1);
+	    this.player.getBoard().moveShip(this.ship, direction);
+	} else if (this.direction == DOWN)
+	{
+	    ship.setRow(ship.getRow() + 1);
+	    this.player.getBoard().moveShip(this.ship, direction);
+	} else if (this.direction == RIGHT)
+	{
+	    ship.setCol(ship.getCol() + 1);
+	    this.player.getBoard().moveShip(this.ship, direction);
+	} else if (this.direction == LEFT)
+	{
+	    ship.setCol(ship.getCol() - 1);
+	    this.player.getBoard().moveShip(this.ship, direction);
+	}
+    }
 
 }
